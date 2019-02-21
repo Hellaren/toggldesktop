620a7103e448c4996c0213b3783d93731f53d082
Nghia Tran <vinhnghiatran@gmail.com>
2019-02-21 17:31:36 +0700

    Support load more cell (mac)

--- src/ui/osx/TogglDesktop/test2/LoadMoreCell.m
+++ src/ui/osx/TogglDesktop/test2/LoadMoreCell.m
@@ -8,31 +8,20 @@
 
 #import "LoadMoreCell.h"
 
+@interface LoadMoreCell ()
+@property (weak) IBOutlet NSProgressIndicator *loader;
+@property (weak) IBOutlet NSButton *loadButton;
+- (IBAction)loadMoreClicked:(id)sender;
+@end
+
 @implementation LoadMoreCell
 extern void *ctx;
 
-- (void)initCell
-{
-	[self setupLoadMoreText];
+- (void)awakeFromNib {
+	[super awakeFromNib];
+
 	[self.loader stopAnimation:nil];
 	[self.loader setHidden:YES];
-}
-
-- (void)setupLoadMoreText
-{
-	NSMutableParagraphStyle *paragrapStyle = NSMutableParagraphStyle.new;
-
-	paragrapStyle.alignment = kCTTextAlignmentCenter;
-
-	NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:@"Load More"];
-	[string setAttributes:
-	 @{
-		 NSFontAttributeName : [NSFont systemFontOfSize:13],
-		 NSForegroundColorAttributeName:[ConvertHexColor hexCodeToNSColor:@"#000000"],
-		 NSParagraphStyleAttributeName:paragrapStyle
-	 }
-					range:NSMakeRange(0, [string length])];
-	self.loadButton.attributedTitle = string;
 }
 
 - (IBAction)loadMoreClicked:(id)sender
